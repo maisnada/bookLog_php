3313b4390da7310e4729f5124b578edc22d6a5ab
Marcelo Moreno <marcelo.moreno@univali.br>
2024-08-20 07:43:20 -0300

    criação chamado

--- index.php
+++ index.php
@@ -6,14 +6,4 @@ require_once __DIR__.'/src/Models/Atividade.php';
 
 $atividade = new Atividade(NULL, 'Teste', new \DateTime(), new \DateTime());
 
-//var_dump($atividade);
-
-//echo $atividade->getInicio()->format('d/m/Y H\hi');
-
 echo $atividade.PHP_EOL;
-
-$intervalo = $atividade->getfim()->diff($atividade->getInicio());
-
-print_r($intervalo);
-
-echo $intervalo->d;
